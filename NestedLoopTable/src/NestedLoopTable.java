66c35edd94c2101184371b07cd3771d51dd519a8
lmacpherson <macpherson@student.ucr.edu>
2019-02-25 10:09:02 -0800

    First commit. Only contains empty operation and driver class.

---
+++ NestedLoopTable/src/NestedLoopTable.java
@@ -0,0 +1,6 @@
+
+public class NestedLoopTable {	//operation class
+
+	
+	//TODO: Create methods to be used in the driver class.
+}
---
+++ NestedLoopTable/src/NestedLoopTableApp.java
@@ -0,0 +1,21 @@
+
+public class NestedLoopTableApp {	//driver class
+
+	public static void main(String[] args) {
+		/*
+			TODO: Create a program that prints out lines of numbers in a "pyramid" formation.
+			i.e.: 
+			1
+			1 2
+			1 2 3 
+			1 2 3 4 
+			1 2 3 4 5
+			1 2 3 4 5 6
+			
+			Using a nested loop. Prompt the user for the first and last numbers to print out, 
+			and continue the app with a (y/n) input from the user. Run the program at least once.
+			Ask for user input through the console, and print the result into the console.
+		*/
+	}
+
+}
