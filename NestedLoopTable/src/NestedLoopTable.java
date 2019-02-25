2ef4dc45e917d4a605d9f7964eba5dd0a3565c12
lmacpherson <macpherson@student.ohlone.edu>
2019-02-25 10:50:46 -0800

    First user input added

--- NestedLoopTable/src/NestedLoopTable.java
+++ NestedLoopTable/src/NestedLoopTable.java
@@ -1,6 +1,12 @@
 
 public class NestedLoopTable {	//operation class
-
+	int firstNum,	
+		lastNum;	
+	
+	public void setNums(int first, int last) {
+		firstNum = first;
+		lastNum = last;
+	}
 	
 	//TODO: Create methods to be used in the driver class.
 	
--- NestedLoopTable/src/NestedLoopTableApp.java
+++ NestedLoopTable/src/NestedLoopTableApp.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class NestedLoopTableApp {	//driver class
 
 	public static void main(String[] args) {
@@ -16,7 +16,26 @@ public class NestedLoopTableApp {	//driver class
 			and continue the app with a (y/n) input from the user. Run the program at least once.
 			Ask for user input through the console, and print the result into the console.
 		*/
+		NestedLoopTable nlt = new NestedLoopTable();	//create an object of NestedLoopTable
+		Scanner input = new Scanner(System.in);
+		String choice = "y";
 		
-	}
+		int first,	//declare int for first and last numbers
+			last;
+		
+		while (choice.equalsIgnoreCase("y")) {	//while loop runs program once, then prompts user to continue or quit
+			//TODO: Prompt user for first and last numbers
+			System.out.print("Enter the first number of the pyramid: ");
+			first = input.nextInt();
+			System.out.print("Enter the last number of the pyramid: ");
+			last = input.nextInt();
+			nlt.setNums(first, last);
 
-}
+			//TODO: pyramid is printed using nested loops
+			System.out.print("\n Print a new pyramid? (y/n)");
+			choice = input.nextLine();
+		}	//end while
+		
+	}	//end main()
+
+}	//end NestedLoopTableApp
